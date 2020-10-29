4156006850dd01335747bfec6118449c4e249abe
David Joy <djoy@edx.org>
2020-10-29 17:01:49 -0400

    Adding boilerplate repository fluff from other repos
    Merging frontend-component-header’s gunk with best practices from frontend-platform.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# frontend-organisms
+[![Build Status](https://api.travis-ci.org/edx/frontend-organisms.svg?branch=master)](https://travis-ci.org/edx/frontend-organisms)
+[![Codecov](https://img.shields.io/codecov/c/github/edx/frontend-organisms)](https://codecov.io/gh/edx/frontend-organisms)
+[![NPM Version](https://img.shields.io/npm/v/@edx/frontend-organisms.svg)](https://www.npmjs.com/package/@edx/frontend-organisms)
+[![npm_downloads](https://img.shields.io/npm/dt/@edx/frontend-organisms.svg)](https://www.npmjs.com/package/@edx/frontend-organisms)
+[![license](https://img.shields.io/npm/l/@edx/frontend-organisms.svg)](https://github.com/edx/frontend-organisms/blob/master/LICENSE)
+[![semantic release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+# Overview
+
+frontend-organisms contains shared [organisms](https://atomicdesign.bradfrost.com/chapter-2/#organisms) used across Open edX micro-frontend applications.
---
+++ src/Header.jsx
@@ -0,0 +1,175 @@
+import React, { useContext } from 'react';
+import Responsive from 'react-responsive';
+import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { sendTrackEvent } from '@edx/frontend-platform/analytics';
+
+import { AppContext } from '@edx/frontend-platform/react';
+import {
+  APP_CONFIG_INITIALIZED,
+  ensureConfig,
+  mergeConfig,
+  getConfig,
+  subscribe,
+} from '@edx/frontend-platform';
+
+import DesktopHeader from './DesktopHeader';
+import MobileHeader from './MobileHeader';
+
+import LogoSVG from './logo.svg';
+
+import messages from './Header.messages';
+
+import useEnterpriseConfig from './data/hooks/enterprise';
+
+ensureConfig([
+  'LMS_BASE_URL',
+  'LOGOUT_URL',
+  'LOGIN_URL',
+  'MARKETING_SITE_BASE_URL',
+  'ORDER_HISTORY_URL',
+], 'Header component');
+
+subscribe(APP_CONFIG_INITIALIZED, () => {
+  mergeConfig({
+    MINIMAL_HEADER: !!process.env.MINIMAL_HEADER,
+    ENTERPRISE_LEARNER_PORTAL_HOSTNAME: process.env.ENTERPRISE_LEARNER_PORTAL_HOSTNAME,
+  }, 'Header additional config');
+});
+
+function Header({ intl }) {
+  const { authenticatedUser, config } = useContext(AppContext);
+  const {
+    enterpriseLearnerPortalLink,
+    enterpriseCustomerBrandingConfig,
+  } = useEnterpriseConfig(authenticatedUser, config);
+
+  const mainMenu = [
+    {
+      type: 'item',
+      href: `${config.LMS_BASE_URL}/dashboard`,
+      content: intl.formatMessage(messages['header.links.courses']),
+    },
+    {
+      type: 'item',
+      href: `${config.LMS_BASE_URL}/dashboard/programs`,
+      content: intl.formatMessage(messages['header.links.programs']),
+    },
+    {
+      type: 'item',
+      href: `${config.MARKETING_SITE_BASE_URL}/course`,
+      content: intl.formatMessage(messages['header.links.content.search']),
+      onClick: () => {
+        sendTrackEvent(
+          'edx.bi.dashboard.find_courses_button.clicked',
+          { category: 'header', label: 'header' },
+        );
+      },
+    },
+  ];
+
+  const dashboardMenuItem = {
+    type: 'item',
+    href: `${config.LMS_BASE_URL}/dashboard`,
+    content: intl.formatMessage(messages['header.user.menu.dashboard']),
+  };
+
+  const logoutMenuItem = {
+    type: 'item',
+    href: config.LOGOUT_URL,
+    content: intl.formatMessage(messages['header.user.menu.logout']),
+  };
+
+  const orderHistoryItem = {
+    type: 'item',
+    href: config.ORDER_HISTORY_URL,
+    content: intl.formatMessage(messages['header.user.menu.order.history']),
+  };
+
+  // If there is an Enterprise LP link, use that instead of the B2C Dashboard
+  let baseUserMenuDashboardLinks = [];
+  if (enterpriseLearnerPortalLink) {
+    baseUserMenuDashboardLinks = [enterpriseLearnerPortalLink];
+  } else {
+    baseUserMenuDashboardLinks = [dashboardMenuItem];
+  }
+
+  let userMenu = authenticatedUser === null ? [] : [
+    ...baseUserMenuDashboardLinks,
+    {
+      type: 'item',
+      href: `${config.LMS_BASE_URL}/u/${authenticatedUser.username}`,
+      content: intl.formatMessage(messages['header.user.menu.profile']),
+    },
+    {
+      type: 'item',
+      href: `${config.LMS_BASE_URL}/account/settings`,
+      content: intl.formatMessage(messages['header.user.menu.account.settings']),
+    },
+    logoutMenuItem,
+  ];
+
+  // Users should only see Order History if they do not have an available
+  // learner portal, because an available learner portal currently means
+  // that they access content via Subscriptions, in which context an "order"
+  // is not relevant.
+  if (!enterpriseLearnerPortalLink) {
+    userMenu.splice(-1, 0, orderHistoryItem);
+  }
+
+  if (getConfig().MINIMAL_HEADER && authenticatedUser !== null) {
+    userMenu = [
+      dashboardMenuItem,
+      logoutMenuItem,
+    ];
+  }
+
+  const loggedOutItems = [
+    {
+      type: 'item',
+      href: config.LOGIN_URL,
+      content: intl.formatMessage(messages['header.user.menu.login']),
+    },
+    {
+      type: 'item',
+      href: `${config.LMS_BASE_URL}/register`,
+      content: intl.formatMessage(messages['header.user.menu.register']),
+    },
+  ];
+
+  let props = {
+    logo: LogoSVG,
+    logoAltText: 'edX',
+    siteName: 'edX',
+    logoDestination: getConfig().MINIMAL_HEADER ? null : `${config.LMS_BASE_URL}/dashboard`,
+    loggedIn: authenticatedUser !== null,
+    username: authenticatedUser !== null ? authenticatedUser.username : null,
+    avatar: authenticatedUser !== null ? authenticatedUser.avatar : null,
+    mainMenu: getConfig().MINIMAL_HEADER ? [] : mainMenu,
+    userMenu,
+    loggedOutItems,
+  };
+
+  if (enterpriseCustomerBrandingConfig) {
+    props = {
+      ...props,
+      ...enterpriseCustomerBrandingConfig,
+    };
+  }
+
+  return (
+    <React.Fragment>
+      <Responsive maxWidth={768}>
+        <MobileHeader {...props} />
+      </Responsive>
+      <Responsive minWidth={769}>
+        <DesktopHeader {...props} />
+      </Responsive>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(Header);
